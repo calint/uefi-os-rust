3b9b31540db66b19e20a017938036d61f7814aff
calint <calin.tenitchi@gmail.com>
2026-02-13 16:23:48 +0100

    .

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,14 @@
-#![no_std]
 #![no_main]
+#![no_std]
 
+use core::time::Duration;
 use log::info;
 use uefi::prelude::*;
 
 #[entry]
 fn main() -> Status {
-    if uefi::helpers::init().is_err() {
-        return Status::ABORTED;
-    }
-
-    info!("Success! Booting with Rust 1.93 and UEFI 0.36.");
-
-    loop {
-        core::hint::spin_loop();
-    }
-}
-
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
-    loop {
-        core::hint::spin_loop();
-    }
+    uefi::helpers::init().unwrap();
+    info!("Hello world!");
+    boot::stall(Duration::from_secs(10));
+    Status::SUCCESS
 }
